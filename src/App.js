072cd49953a185e58528cc7d488e230b8b0fbbfa
Kemelbek <kemelbekirsaliev@gmail.com>
2019-11-01 15:10:37 +0600

    Axios request added + more users + got rid of Users' state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { Component } from "react"
 import "./App.css"
 import Navbar from "./components/layout/Navbar"
 import Users from "./components/users/Users"
+import axios from "axios"
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({ ...this.state, loading: true })
+    const res = await axios.get("https://api.github.com/users")
+    this.setState({ ...this.state, users: res.data, loading: false })
+  }
+
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <Navbar />
-        <div className='container'>
-          <Users />
+        <div className="container">
+          <Users loading={this.state.login} users={this.state.users} />
         </div>
       </div>
     )
